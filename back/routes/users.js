a2ca0be0c5655a35634730e3268671f6458bafb5
Mattia Denti <mattia.denti@acelvia.com>
2019-01-14 17:05:53 +0200

    Back: fix router bug

--- back/routes/users.js
+++ back/routes/users.js
@@ -7,7 +7,7 @@ function getUsersRouter(ctx) {
 
   router.post('/', async (req, res) => {
     try {
-      const userId = await users.createNewUser(ctx, res.body);
+      const userId = await users.createNewUser(ctx, req.body);
       res.send({ userId });
     } catch (error) {
       res.status(500).send({ error: 'could not create new user' });

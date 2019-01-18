aba9b02d12c3b06ef321e8d8df320dbd47c1c533
Mattia Denti <mattia.denti@acelvia.com>
2019-01-18 15:16:07 +0200

    Front: redirect to previous route after authentication

--- front/src/routes/LoggedInRoute.js
+++ front/src/routes/LoggedInRoute.js
@@ -5,7 +5,7 @@ import { userContextConsumer } from '../contexts/UserContext';
 
 function LoggedInRoute({ userCtx, ...props }) {
   if (!userCtx.user) {
-    return <Redirect to="/" />;
+    return <Redirect to={{ pathname: '/', state: { from: props.location } }} />;
   }
 
   return <Route {...props} />;
--- front/src/routes/LoggedOutRoute.js
+++ front/src/routes/LoggedOutRoute.js
@@ -3,9 +3,10 @@ import { Route, Redirect } from 'react-router-dom';
 
 import { userContextConsumer } from '../contexts/UserContext';
 
-function LoggedOutRoute({ userCtx, ...props }) {
+function LoggedOutRoute({ userCtx, location, ...props }) {
+  const { state: { from } } = location || { state: { from: { pathname: '/recipes' } } };
   if (userCtx.user) {
-    return <Redirect to="/recipes" />;
+    return <Redirect to={from} />;
   }
 
   return <Route {...props} />;

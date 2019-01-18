1dbd6b384bd770769d3dce063ccce620d12d3fee
Mattia Denti <mattia.denti@acelvia.com>
2019-01-18 17:19:13 +0200

    Back: automatically transform column names between snake and camel case

--- back/knexfile.js
+++ back/knexfile.js
@@ -1,4 +1,21 @@
-// Update with your config settings.
+function snakeToCamelCase(value) {
+  return value.replace(/_\w/g, m => m[1].toUpperCase());
+}
+
+function objectSnakeToCamelCaseProperties(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(row => objectSnakeToCamelCaseProperties(row));
+  }
+
+  return Object.keys(obj).reduce((prevObj, prop) => {
+    const newObj = Object.assign({}, prevObj, { [snakeToCamelCase(prop)]: obj[prop] });
+    return newObj;
+  }, {});
+}
+
+function camelToSnakeCase(value) {
+  return value.replace(/[A-Z]/g, m => `_${m.toLowerCase()}`);
+}
 
 module.exports = {
   development: {
@@ -9,5 +26,7 @@ module.exports = {
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
     },
+    wrapIdentifier: (value, origImpl) => origImpl(camelToSnakeCase(value)),
+    postProcessResponse: result => objectSnakeToCamelCaseProperties(result),
   },
 };

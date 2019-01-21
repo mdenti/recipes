ac8c8b1587db52243dd39af0eb35d556c35404de
Mattia Denti <mattia.denti@acelvia.com>
2019-01-21 14:54:17 +0200

    Front: small header fix

--- front/src/Header.js
+++ front/src/Header.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 import { AppBar, Toolbar, Button } from '@material-ui/core';
 
 import { userContextConsumer } from './contexts/UserContext';
 
 function Header({ userCtx: { user } }) {
-  const loggedInNavigation = [
-    <Button to="/recipes" component={NavLink}>Recipes</Button>,
-    <Button to="/recipe/create" component={NavLink}>Create new recipe</Button>,
-  ];
-  const loggedOutNavigation = [
-    <Button to="/" component={NavLink}>Login</Button>,
-    <Button to="/user/register" component={NavLink}>Register</Button>,
-  ];
+  const loggedInNavigation = (
+    <Fragment>
+      <Button to="/recipes" component={NavLink}>Recipes</Button>
+      <Button to="/recipe/create" component={NavLink}>Create new recipe</Button>
+    </Fragment>
+  );
+  const loggedOutNavigation = (
+    <Fragment>
+      <Button to="/" component={NavLink}>Login</Button>
+      <Button to="/user/register" component={NavLink}>Register</Button>
+    </Fragment>
+  );
   return (
     <AppBar position="static" color="default" className="header">
       <Toolbar variant="dense">
